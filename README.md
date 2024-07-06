0fa701ce33aa4ad53a78d739cb55fe2fde9c92f1
Csaba Sallai <68144659+csabika98@users.noreply.github.com>
2024-07-06 02:41:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## REACT + VITE + LUA(LAPIS)
+
+Windows supported (WSL)
+
+## Screenshots
+
+![](screenshots/8.png)
+![](screenshots/9.png)
+![](screenshots/5.png)
+![](screenshots/6.png)
+![](screenshots/7.png)
+
+
+# Install
+
+Run both powershell script (you don't need to install anything manually, pwsh script will enable & install WSL, downloading install LUA/Luarocks.. Lapis..)
+```powershell
+powershell -ExecutionPolicy ByPass -File .\install_part1.ps1 
+```
+(WSL install & enable part, it requires admin previliges)
+```powershell
+powershell -ExecutionPolicy ByPass -File .\install_part2.ps1 
+```
+# RUN
+To run the server simply execute the following command:
+```
+npm run dev
+```
---
+++ proxy-server.mjs
@@ -0,0 +1,81 @@
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { spawn } from 'child_process';
+import path from 'path';
+import process from 'process';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function findProjectRoot(currentDir, targetFolderName) {
+    const root = path.parse(currentDir).root;
+    while (currentDir !== root) {
+        let possiblePath = path.join(currentDir, targetFolderName);
+        if (fs.existsSync(possiblePath)) {
+            return currentDir; 
+        }
+        currentDir = path.dirname(currentDir);
+    }
+    return null; 
+}
+
+const projectRoot = findProjectRoot(__dirname, 'ReactViteLua');
+const LuaProjectPath = path.join(projectRoot, 'ReactViteLua');
+console.log(`RUN Lua project at path: ${LuaProjectPath}`);
+
+let LuaProcess;
+
+const runLuaApp = () => {
+    if (LuaProcess) {
+        console.log('Stopping existing Lua application...');
+        LuaProcess.kill(); 
+        LuaProcess = null;
+    }
+    console.log('Starting Lua application...');
+    console.log('Executing command: lapis server');
+
+    // VaporProcess = spawn('wsl', ['./run_vapor.sh'], { cwd: VaporProjectPath });
+
+    LuaProcess = spawn('wsl', ['./run_lua.sh'],{ cwd: LuaProjectPath  });
+    LuaProcess.stdout.on('data', (data) => console.log(`Lua: ${data}`));
+    LuaProcess.stderr.on('data', (data) => console.error(`Lua : ${data}`));
+    LuaProcess.on('close', (code) => {
+        console.log(`Lua process exited with code ${code}`);
+    });    
+};
+
+
+runLuaApp();
+
+app.use((req, res, next) => {
+    console.log(`Request received: ${req.method} ${req.url}`);
+    next();
+});
+
+app.use('/api/', createProxyMiddleware({
+    target: 'http://localhost:8081/',
+    changeOrigin: true,
+    ws: true
+}));
+
+app.use('/', createProxyMiddleware({
+    target: 'http://localhost:5173/',
+    changeOrigin: true,
+    ws: false
+}));
+
+const PORT = 8888;
+app.listen(PORT, () => {
+    console.log(`Proxy server running on http://localhost:${PORT}`);
+});
+
+process.on('exit', () => {
+    if (LuaProcess) {
+        console.log('Stopping Lua application due to script exit...');
+        LuaProcess.kill();
+    }
+});
